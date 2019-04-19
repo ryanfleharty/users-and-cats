e3638bc401c4b0657270400bd8907bfc4e671814
Ryan Fleharty <ryan.fleharty@gmail.com>
2019-04-19 13:33:13 -0600

    added relationships to schema

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
+    cats: [{type: mongoose.Schema.Types.ObjectId, ref: 'Cat'}]
 })
 
 const User = mongoose.model('User', userSchema);
